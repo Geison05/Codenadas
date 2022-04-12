8b0757c547776edc2b61c6b452fea2bc9446b5b9
Geison Amorim <geisonam43@gmail.com>
2022-04-12 10:36:15 -0300

    Add project files.

---
+++ Codenadas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            double X, Y;
+
+
+            //Entrada
+            string[] valor = Console.ReadLine().Split(' ');
+            X = double.Parse(valor[0], CultureInfo.InvariantCulture);
+            Y = double.Parse(valor[1], CultureInfo.InvariantCulture);
+
+            //Processamento / Saida
+
+            if (X == 0.0 || Y == 0.0)
+                Console.WriteLine("Origem");
+
+            else if (X == 0.0)
+                Console.WriteLine("Eixo Y");
+
+            else if (Y == 0.0)
+                Console.WriteLine("Eixo X");
+
+            else if (X > 0.0 && Y > 0.0)
+                Console.WriteLine("Q1");
+
+            else if (X < 0.0 && Y > 0.0)
+                Console.WriteLine("Q2");
+
+            else if (X < 0.0 && Y < 0.0)
+                Console.WriteLine("Q3");
+
+            else
+                Console.WriteLine("Q4");
+
+        }
+    }
+}
